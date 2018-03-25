04277db581971aedcdbbb6ccbe7a563a10a02b10
knez <luka.knezovic0@gmail.com>
2018-03-25 18:40:03 +0200

    Storing value in db

---
+++ app/src/main/java/hr/lknezovic/duptraningprogram/Lift.java
@@ -0,0 +1,47 @@
+package hr.lknezovic.duptraningprogram;
+
+import android.widget.EditText;
+import android.widget.TextView;
+
+/**
+ * Created by knez on 03/25/18.
+ */
+
+public class Lift {
+
+    private String id;
+    private String name;
+    private Double weight;
+
+
+    public Lift(String id, String name, Double weight) {
+        this.id = id;
+        this.name = name;
+        this.weight = weight;
+    }
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+}
--- app/src/main/java/hr/lknezovic/duptraningprogram/Main.java
+++ app/src/main/java/hr/lknezovic/duptraningprogram/Main.java
@@ -10,14 +10,19 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.concurrent.TimeUnit;
 
 public class Main extends AppCompatActivity {
 
     private Button button1, button2;
-    private TextView result, countDowntimer;
-    private EditText weight;
+    private TextView result, countDowntimer, nameView;
+    private EditText weightText;
     private CountDownTimer timer;
+    DatabaseReference ref;
 
 
     @Override
@@ -25,14 +30,16 @@ public class Main extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final long bootTime = SystemClock.elapsedRealtime();
 
-
+        nameView = (TextView) findViewById(R.id.Squat);
         button1 = (Button) findViewById(R.id.button);
         button2 = (Button) findViewById(R.id.button2);
         result = (TextView) findViewById(R.id.result);
-        weight = (EditText) findViewById(R.id.weight);
+        weightText = (EditText) findViewById(R.id.weight);
         countDowntimer = (TextView) findViewById(R.id.timer);
+
+
+        ref = FirebaseDatabase.getInstance().getReference("lifts");
 
 
         button1.setOnClickListener(new View.OnClickListener() {
@@ -42,13 +49,13 @@ public class Main extends AppCompatActivity {
 
 
                 try {
-                    String currentWeight = weight.getText().toString();
+                    String currentWeight = weightText.getText().toString();
                     Float oldWeight = Float.parseFloat(currentWeight);
                     Double newWeight = oldWeight + 2.5;
 
                     if (b.equals("")) {
                         button1.setText("5");
-                        //result.setText("Congrats! Next time pick up" + newWeight + "kg");
+
                         Toast.makeText(getApplicationContext(), "Congrats! Rest 3 minutes",
                                 Toast.LENGTH_LONG).show();
 
@@ -111,7 +118,7 @@ public class Main extends AppCompatActivity {
                 String b2 = button2.getText().toString();
 
                 try {
-                    String currentWeight = weight.getText().toString();
+                    String currentWeight = weightText.getText().toString();
                     Float oldWeight = Float.parseFloat(currentWeight);
                     Double newWeight = oldWeight + 2.5;
 
@@ -174,6 +181,14 @@ public class Main extends AppCompatActivity {
         });
 
 
+    }
+
+    public void addWeight(Double weight) {
+        String name = nameView.getText().toString().trim();
+        String id = ref.push().getKey();
+
+        Lift lift = new Lift(id, name, weight);
+        ref.child(id).setValue(lift);
 
 
     }
@@ -181,19 +196,22 @@ public class Main extends AppCompatActivity {
     public Boolean checkButtonValues(Button button1, Button button2) {
         String b1 = button1.getText().toString();
         String b2 = button2.getText().toString();
-        String currentWeight = weight.getText().toString();
+        String currentWeight = weightText.getText().toString();
         Float oldWeight = Float.parseFloat(currentWeight);
         Double newWeight = oldWeight + 2.5;
 
         try {
             if (b1.equals("5") && b2.equals("5") && !b1.equals("") && !b2.equals("")) {
                 result.setText("You strong!" + " Next time pick up " + newWeight + "kg");
+                addWeight(newWeight);
                 return true;
             } else if (b1.equals("") || b2.equals("")) {
                 result.setText("");
 
             } else
                 result.setText("Dude, get stronger. Next time pick up " + currentWeight + "kg");
+                Double oldWright = Double.parseDouble(currentWeight);
+                addWeight(oldWright);
         } catch (NumberFormatException e) {
 
         }
@@ -204,7 +222,7 @@ public class Main extends AppCompatActivity {
         timer = new CountDownTimer(60 * 3000, 1000) {
             @Override
             public void onTick(long millisUntilFinished) {
-                countDowntimer.setText("" + "If it was easy, rest for 1 minute and 30 seconds \n" +String.format("%d minutes, %d seconds",
+                countDowntimer.setText("" + "If it was easy, rest for 1 minute and 30 seconds \n" + String.format("%d minutes, %d seconds",
                         TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished),
                         TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) -
                                 TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished))));
@@ -236,7 +254,7 @@ public class Main extends AppCompatActivity {
 
     public void stopTimer() {
 
-        if(countDowntimer != null) {
+        if (countDowntimer != null) {
             timer.cancel();
         }
     }
